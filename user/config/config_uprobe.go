3f7659004fff02a3568c4969fd86ecd5edf2e32f
see <seeflower.dev@gmail.com>
2023-07-21 09:21:11 +0800

    bug fix

--- user/config/config_uprobe.go
+++ user/config/config_uprobe.go
@@ -8,7 +8,7 @@ type UArgs = UprobeArgs
 
 func init() {
 
-	libc_path := "/apex/com.android.runtime/lib64/bionic/libc.so"
+	// libc_path := "/apex/com.android.runtime/lib64/bionic/libc.so"
 
-	Register(&UArgs{libc_path, PA("strstr", []PArg{A("haystack", STRING), A("needle", STRING)})})
+	// Register(&UArgs{libc_path, PA("strstr", []PArg{A("haystack", STRING), A("needle", STRING)})})
 }
